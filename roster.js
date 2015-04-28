ac1ff5395b8d9dcb0e226bb3446e1ecd8399ca3c
Martin Thomson <martin.thomson@gmail.com>
2015-04-28 15:52:50 -0700

    Moving require.js into a compartment of its own

--- roster.js
+++ roster.js
@@ -1,10 +1,14 @@
-define(['require', 'util', 'entity', 'policy'], function(require) {
+define(['require', 'util', 'entity', 'policy', 'rosterop'], function(require) {
   'use strict';
 
   var c = crypto.subtle;
   var util = require('util');
   var EntityPolicy = require('policy');
   var PublicEntity = require('entity').PublicEntity;
+  var RosterOperation = require('rosterop').RosterOperation;
+  var RosterOpcode = require('rosterop').RosterOpcode;
+  var ChangeOperation = require('rosterop').ChangeOperation;
+  var ShareOperation = require('rosterop').ShareOperation;
 
   var HASH = 'SHA-256';
   // Hash some junk and then change the output to zeros.  Which allows for hash
@@ -41,98 +45,17 @@ define(['require', 'util', 'entity', 'policy'], function(require) {
   };
   var allRosters = new AllRosters();
 
-  function RosterOpcode(op) {
-    this.opcode = op;
-  }
-  RosterOpcode.prototype = {
-    encode: function() {
-      return new Uint8Array([this.opcode]);
-    },
-    equals: function(other) {
-      return other instanceof RosterOpcode &&
-        this.opcode === other.opcode;
-    }
-  };
-  RosterOpcode.decode = function(buf) {
-    return new RosterOpcode(new Uint8Array(buf)[0]);
-  };
-  RosterOpcode.CHANGE = new RosterOpcode(0);
-  RosterOpcode.SHARE = new RosterOpcode(1);
-  RosterOpcode.CHANGE = new RosterOpcode(2);
-
-  /**
-   * The base implementation of an operation on the roster.
-   */
-  function RosterOperation(actor) {
-    this.actor = actor;
-  }
-
-  /** Encodes this.  This takes a promise to the hash of the previous entry. */
-  RosterOperation.prototype = {
-    _encodeParts: function() {
-      throw new Error('not implemented');
-    },
-    encode: function(lastEntryHash) {
-      var pieces = [].concat(
-        this.opcode.encode(),
-        this._encodeParts(),
-        this.actor.identity,
-        lastEntryHash
-      );
-
-      return Promise.all(pieces)
-        .then(encodedPieces => {
-          var msg = util.bsConcat(encodedPieces);
-          return this.actor.sign(msg)
-            .then(sig => {
-              return util.bsConcat([msg, sig]);
-            });
-        });
-    }
-  };
-
-  function ChangeOperation(actor, subject, policy) {
-    RosterOperation.call(this, actor);
-    this.opcode = RosterOpcode.CHANGE;
-    this.subject = subject;
+  /** Used internally by the roster to track the status of entities in the roster */
+  function CacheEntry(subject, policy) {
+    PublicEntity.call(this, subject.identity, subject.share);
     this.policy = policy;
   }
-  ChangeOperation.prototype = util.mergeDict({
-    _encodeParts: function() {
-      return [ this.subject.identity, this.policy.encode() ];
-    }
-  }, Object.create(RosterOperation.prototype));
-
-  function ShareOperation(actor) {
-    RosterOperation.call(this, actor);
-    this.opcode = RosterOpcode.SHARE;
-    this.subject = actor;
-  }
-  ShareOperation.prototype = util.mergeDict({
-    _encodeParts: function() {
-      return [ this.subject.encodeShare() ];
-    }
-  }, Object.create(RosterOperation.prototype));
-
-  /** For a user roster, changes need to identify the actor AND the roster that
-   * they are acting for. The roster is used to determine whether the action is
-   * permitted. The actor is used to provide the signing public key; the actor
-   * also needs to be a member on the roster. */
-  function RosterChangeOperation(actor, actorRoster, subject, policy) {
-    ChangeOperation.call(this, actor, subject, policy);
-    this.opcode = RosterOpcode.CHANGE_ROSTER;
-    this.actorRoster = actorRoster;
-  }
-  RosterChangeOperation.prototype = util.mergeDict({
-    _encodeParts: function() {
-      return [ this.subject.encodeShare(), this.roster.encodeShare() ];
-    }
-  }, Object.create(ChangeOperation.prototype));
+  CacheEntry.prototype = Object.create(PublicEntity.prototype);
 
   // Calculate some lengths based on the same principle: generate a real value
   // and work out how big it is.  When the algorithm details are nailed down,
   // this won't be necessary.
-  RosterOperation.lengths = (function() {
+  var itemLengths = (function() {
     return PublicEntity.lengths.then(
       entityLengths => util.promiseDict(
         util.mergeDict(entityLengths, {
@@ -142,43 +65,6 @@ define(['require', 'util', 'entity', 'policy'], function(require) {
         }))
     );
   }());
-
-  RosterOperation.decode = function(parser, lengths) {
-    var opcode = RosterOpcode.decode(parser.next(lengths.opcode));
-    var subject, actor, policy;
-
-    if (opcode.equals(RosterOpcode.CHANGE)) {
-      subject = new PublicEntity(parser.next(lengths.identifier));
-      policy = EntityPolicy.decode(parser.next(lengths.policy));
-      actor = new PublicEntity(parser.next(lengths.identifier));
-      return Promise.resolve(new ChangeOperation(actor, subject, policy));
-    }
-    if (opcode.equals(RosterOpcode.SHARE)) {
-      var share = parser.next(lengths.share);
-      actor = new PublicEntity(parser.next(lengths.identifier), share);
-      return Promise.resolve(new ShareOperation(actor));
-    }
-    if (opcode.equals(RosterOpcode.CHANGE_ROSTER)) {
-      subject = allRosters.lookup(parser.next(lengths.identifier));
-      policy = EntityPolicy.decode(parser.next(lengths.policy));
-      actor = new PublicEntity(parser.next(lengths.identifier));
-      var actorRoster = allRosters.lookup(parser.next(lengths.identifier));
-      return util.promiseDict({
-        subject: subject,
-        actorRoster: actorRoster
-      }).then(r => new RosterChangeOperation(actor, r.actorRoster,
-                                             r.subject, policy));
-    }
-    return Promise.reject(new Error('invalid operation: ' + opcode.opcode));
-  };
-
-  /** Used internally by the roster to track the status of entities in the roster */
-  function CacheEntry(subject, policy) {
-    PublicEntity.call(this, subject.identity, subject.share);
-    this.policy = policy;
-  }
-  CacheEntry.prototype = Object.create(PublicEntity.prototype);
-
 
   /** Creates a roster. */
   function Roster() {
@@ -250,7 +136,7 @@ define(['require', 'util', 'entity', 'policy'], function(require) {
      */
     _decodeAndAdd: function(parser) {
       var startPosition = parser.position;
-      return RosterOperation.lengths.then(
+      return itemLengths.then(
         lengths => RosterOperation.decode(parser, lengths)
           .then(op => {
             var hash = parser.next(lengths.hash);
---
+++ rosterop.js
@@ -0,0 +1,133 @@
+define(['require', 'util', 'entity', 'policy'], function(require) {
+  'use strict';
+
+  var util = require('util');
+  var EntityPolicy = require('policy');
+  var PublicEntity = require('entity').PublicEntity;
+
+  function RosterOpcode(op) {
+    this.opcode = op;
+  }
+  RosterOpcode.prototype = {
+    encode: function() {
+      return new Uint8Array([this.opcode]);
+    },
+    equals: function(other) {
+      return other instanceof RosterOpcode &&
+        this.opcode === other.opcode;
+    }
+  };
+  RosterOpcode.decode = function(buf) {
+    return new RosterOpcode(new Uint8Array(buf)[0]);
+  };
+  RosterOpcode.CHANGE = new RosterOpcode(0);
+  RosterOpcode.SHARE = new RosterOpcode(1);
+  RosterOpcode.CHANGE = new RosterOpcode(2);
+
+  /**
+   * The base implementation of an operation on the roster.
+   */
+  function RosterOperation(actor) {
+    this.actor = actor;
+  }
+
+  /** Encodes this.  This takes a promise to the hash of the previous entry. */
+  RosterOperation.prototype = {
+    _encodeParts: function() {
+      throw new Error('not implemented');
+    },
+    encode: function(lastEntryHash) {
+      var pieces = [].concat(
+        this.opcode.encode(),
+        this._encodeParts(),
+        this.actor.identity,
+        lastEntryHash
+      );
+
+      return Promise.all(pieces)
+        .then(encodedPieces => {
+          var msg = util.bsConcat(encodedPieces);
+          return this.actor.sign(msg)
+            .then(sig => {
+              return util.bsConcat([msg, sig]);
+            });
+        });
+    }
+  };
+
+  function ChangeOperation(actor, subject, policy) {
+    RosterOperation.call(this, actor);
+    this.opcode = RosterOpcode.CHANGE;
+    this.subject = subject;
+    this.policy = policy;
+  }
+  ChangeOperation.prototype = util.mergeDict({
+    _encodeParts: function() {
+      return [ this.subject.identity, this.policy.encode() ];
+    }
+  }, Object.create(RosterOperation.prototype));
+
+  function ShareOperation(actor) {
+    RosterOperation.call(this, actor);
+    this.opcode = RosterOpcode.SHARE;
+    this.subject = actor;
+  }
+  ShareOperation.prototype = util.mergeDict({
+    _encodeParts: function() {
+      return [ this.subject.encodeShare() ];
+    }
+  }, Object.create(RosterOperation.prototype));
+
+  /** For a user roster, changes need to identify the actor AND the roster that
+   * they are acting for. The roster is used to determine whether the action is
+   * permitted. The actor is used to provide the signing public key; the actor
+   * also needs to be a member on the roster. */
+  function RosterChangeOperation(actor, actorRoster, subject, policy) {
+    ChangeOperation.call(this, actor, subject, policy);
+    this.opcode = RosterOpcode.CHANGE_ROSTER;
+    this.actorRoster = actorRoster;
+  }
+  RosterChangeOperation.prototype = util.mergeDict({
+    _encodeParts: function() {
+      return [ this.subject.encodeShare(), this.roster.encodeShare() ];
+    }
+  }, Object.create(ChangeOperation.prototype));
+
+
+  RosterOperation.decode = function(parser, lengths) {
+    var opcode = RosterOpcode.decode(parser.next(lengths.opcode));
+    var subject, actor, policy;
+
+    if (opcode.equals(RosterOpcode.CHANGE)) {
+      subject = new PublicEntity(parser.next(lengths.identifier));
+      policy = EntityPolicy.decode(parser.next(lengths.policy));
+      actor = new PublicEntity(parser.next(lengths.identifier));
+      return Promise.resolve(new ChangeOperation(actor, subject, policy));
+    }
+    if (opcode.equals(RosterOpcode.SHARE)) {
+      var share = parser.next(lengths.share);
+      actor = new PublicEntity(parser.next(lengths.identifier), share);
+      return Promise.resolve(new ShareOperation(actor));
+    }
+    if (opcode.equals(RosterOpcode.CHANGE_ROSTER)) {
+      subject = allRosters.lookup(parser.next(lengths.identifier));
+      policy = EntityPolicy.decode(parser.next(lengths.policy));
+      actor = new PublicEntity(parser.next(lengths.identifier));
+      var actorRoster = allRosters.lookup(parser.next(lengths.identifier));
+      return util.promiseDict({
+        subject: subject,
+        actorRoster: actorRoster
+      }).then(r => new RosterChangeOperation(actor, r.actorRoster,
+                                             r.subject, policy));
+    }
+    return Promise.reject(new Error('invalid operation: ' + opcode.opcode));
+  };
+
+  return {
+    ChangeOperation: ChangeOperation,
+    RosterChangeOperation: RosterChangeOperation,
+    RosterOpcode: RosterOpcode,
+    RosterOperation: RosterOperation,
+    ShareOperation: ShareOperation
+  };
+});
